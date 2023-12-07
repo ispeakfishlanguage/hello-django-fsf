7b88baefc0bb76c8083223005dfd970f535d5ecd
ispeakfishlang <sandleguiz@gmail.com>
2023-12-07 23:23:32 +0100

    Fix syntax error

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-!d!em(xvl_mv3y2bu!r2$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('HEROKU_HOSTNAME')
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
