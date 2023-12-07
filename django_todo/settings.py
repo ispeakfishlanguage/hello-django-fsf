83488112a0fba75df051d3d4d4f2c6e7f77733df
ispeakfishlang <sandleguiz@gmail.com>
2023-12-07 23:38:11 +0100

    Removed secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    "SECRET_KEY", "django-insecure-!d!em(xvl_mv3y2bu!r2$xxmo96h#&(e*$n9a4f6y=_mni*#g5"
+    "SECRET_KEY", ""
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
